e0701269d3fd63dcacc5faffe88105676a1415d4
Erik <erik@Mac.lan>
2025-05-30 10:47:05 -0400

    removed unused imports

--- src/app/IncludeCommentsInput.tsx
+++ src/app/IncludeCommentsInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { STORAGE_KEY_INCLUDE_COMMENTS } from "./constants";
 
 interface IncludeCommentsInputProps {
